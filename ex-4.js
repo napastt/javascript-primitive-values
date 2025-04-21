7e544106d6f028a9c45daec2d695a4f0c91c2c73
Napas T. <napassorn.ti@gmail.com>
2025-04-21 18:50:16 +0700

    Modified ex-4

--- ex-4.js
+++ ex-4.js
@@ -4,4 +4,4 @@
 let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
 
-console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
+console.log((numberOfSoldProduct / numberOfProductInStock) * 100); // numberOfProductInStock is undefined -> which is not a number, so the result is NaN.
